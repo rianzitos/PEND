61f949bb0718f91d741b0a7fa2d5172e59dc7e73
rianzitos <rianrafael063@gmail.com>
2026-08-13 12:03:07 -0300

    Atividades práticas OO com DOM

---
+++ JavaScript/OO/13082026/Atividade Pratica/script.js
@@ -0,0 +1,67 @@
+class Produto {
+
+    constructor(nome, preco, categoria, desconto) {
+
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.desconto = desconto;
+    }
+
+    aplicarDesconto() {
+        const precoDesconto = this.preco - (this.preco * this.desconto / 100);
+
+        return precoDesconto;
+    }
+
+
+}
+
+class Estoque {
+
+    constructor() {
+        this.produtos = [];
+    }
+
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+    }
+
+    exibir() {
+
+        const resultado = document.querySelector("#resultado")
+
+        resultado.innerHTML = "";
+
+        this.produtos.forEach(produto => {
+
+            resultado.innerHTML += `
+            <div>            
+                <p>${produto.nome}</p>
+                <p>Preço: R$${produto.aplicarDesconto(produto)}</p>
+                <p>Categoria: ${produto.categoria}</p>
+                <p>Desconto: ${produto.desconto}%</p>
+            </div>
+        `;
+        
+        })
+    }
+}
+
+const estoque = new Estoque();
+const nome = document.querySelector("#nome");
+const preco = document.querySelector("#preco");
+const categoria = document.querySelector("#categoria");
+const desconto = document.querySelector("#desconto");
+const botaoCadastrar = document.querySelector("#botaoCadastrar");
+
+botaoCadastrar.addEventListener("click", function () {
+
+    const produto = new Produto(nome.value, preco.value, categoria.value, desconto.value);
+
+    estoque.adicionarProduto(produto);
+    estoque.exibir();
+});
+
+
+
